92c2d9889e464cb1849d46bc2f07c50ad299b4be
zhangbing <zhangbing@kaike.la>
2017-10-12 23:07:02 +0800

    this指向

---
+++ public/test.js
@@ -0,0 +1,9 @@
+var animal=function(){
+    name:'animal',
+    yell : function(){
+        console.log(this.name);
+    }
+}
+var dog=function(){
+    yell:
+}
--- src/module.js
+++ src/module.js
@@ -2,7 +2,7 @@
 const HELLO="Hello word!";
 var hello= function(name) {
     var greet = document.createElement('h1');
-    greet.textContent =`HELLO${HELLO}`;
+    greet.textContent =`${HELLO}${name}`;
     return greet;
 }
 module.exports = {
