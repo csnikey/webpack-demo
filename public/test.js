35d8448647936921afa86c00d002a5a79fd34fcc
zhangbing <zhangbing@kaike.la>
2017-10-13 09:32:57 +0800

    提交

--- public/test.js
+++ public/test.js
@@ -1,9 +1,15 @@
-var animal=function(){
-    name:'animal',
-    yell : function(){
-        console.log(this.name);
+name="test";
+var person = {
+    name: 'Abby',
+    sayName: function() {
+        var fn = function(){
+            return function() {
+                console.log(this);   //window
+                console.log(this.name); //Abby
+            }
+        }
+        fn()();
     }
 }
-var dog=function(){
-    yell:
-}
+
+person.sayName();
